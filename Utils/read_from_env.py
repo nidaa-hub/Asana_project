6295d807ee891b68876e8b7f694f47cb328e9453
kanj-as <nidaa.abo.salh@gmail.com>
2024-03-26 16:56:59 +0200

    adding jira

--- Utils/read_from_env.py
+++ Utils/read_from_env.py
@@ -7,15 +7,15 @@ load_dotenv()
 class Credentials:
 
     def get_email(self):
-    #    email = os.getenv("EMAIL")
-        email = os.environ["EMAIL"]
+        email = os.getenv("EMAIL")
+      #  email = os.environ["EMAIL"]
         if email is None:
             raise ValueError("EMAIL_TXT environment variable is not set")
         return email
 
     def get_password(self):
-      #  password = os.getenv("PASSWORD")
-        password = os.environ["PASSWORD"]
+        password = os.getenv("PASSWORD")
+      #  password = os.environ["PASSWORD"]
         if password is None:
             raise ValueError("PASSWORD environment variable is not set")
         return password
@@ -31,3 +31,27 @@ class Credentials:
         if apikey is None:
             raise ValueError("apikey environment variable is not set")
         return apikey
+
+    def get_jira_token(self):
+        jira_token = os.getenv("JIRA_TOKEN")
+        if jira_token is None:
+            raise ValueError("JIRA_TOKEN environment variable is not set")
+        return jira_token
+
+    def get_jira_url(self):
+        jira_url = os.getenv("JIRA_URL")
+        if jira_url is None:
+            raise ValueError("JIRA_URL environment variable is not set")
+        return jira_url
+
+    def get_jira_email(self):
+        jira_email = os.getenv("JIRA_EMAIL")
+        if jira_email is None:
+            raise ValueError("Email environment variable is not set")
+        return jira_email
+
+    def get_jira_project_key(self):
+        jira_project_key = os.getenv("JIRA_PROJECT_KEY")
+        if jira_project_key is None:
+            raise ValueError("jira project key environment variable is not set")
+        return jira_project_key
