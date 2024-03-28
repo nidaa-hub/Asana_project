ab2fa68231f6fe5c673def3c3398c2312ab3f232
kanj-as <nidaa.abo.salh@gmail.com>
2024-03-28 12:32:28 +0200

    update

--- Infra/browser_wrapper.py
+++ Infra/browser_wrapper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from Utils.config_loader import ConfigLoader
 
+
 class BrowserWrapper:
 
     def __init__(self):
@@ -9,17 +10,34 @@ class BrowserWrapper:
         self.config = self.configloader.load_config()
         print("test start")
 
+    def create_options(self, browser_type):
+        if browser_type.lower() == 'chrome':
+            options = webdriver.ChromeOptions()
+        elif browser_type.lower() == 'firefox':
+            options = webdriver.FirefoxOptions()
+        elif browser_type.lower() == 'edge':
+            options = webdriver.EdgeOptions()
+        else:
+            raise ValueError(f"Unsupported browser type: {browser_type}")
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        return options
+
     def get_driver(self, browser='chrome'):
         if self.config["grid"]:
             options = self.set_up_capabilities(browser)
             self.driver = webdriver.Remote(command_executor=self.config["hub"], options=options)
         else:
+            options = self.create_options(browser)
+
             if browser.lower() == 'chrome':
-                self.driver = webdriver.Chrome()
+                self.driver = webdriver.Chrome(options=options)
             elif browser.lower() == 'firefox':
-                self.driver = webdriver.Firefox()
+                self.driver = webdriver.Firefox(options=options)
             elif browser.lower() == 'edge':
-                self.driver = webdriver.Edge()
+                self.driver = webdriver.Edge(options=options)
+
         url = self.config["url"]
         self.driver.get(url)
         self.driver.maximize_window()
@@ -36,7 +54,9 @@ class BrowserWrapper:
         if options is not None:
             platform_name = self.config["platform"]
             options.add_argument(f'--platformName={platform_name}')
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
             return options
         else:
             raise ValueError("Unsupported browser type")
-
